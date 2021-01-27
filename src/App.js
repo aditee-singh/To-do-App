4074f0eb52583e3700e25c83d6cbbb96e10f4ed3
Aditee Singh <singhaditee33@gmail.com>
2021-01-27 16:46:56 +0530

    Modified file system and Added base structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './App.module.css';
-import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className={classes.App}>
-      <h1>Hello World</h1>
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newItem: "",
+      list:[]
+    }
+  }
+  render(){
+    return (
+      <div className={classes.App}>
+        <div>
+          <p>Add an item</p>
+          <br/>
+          <input type="text" placeholder="Enter a task" 
+          onChange={e=> this.updateInput("newItem",e.target.value)}
+          />
+          <button onClick={()=>this.addItem()}>Add</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
