922b697e209478a7e63f5262bf5b30751910c934
Aditee Singh <singhaditee33@gmail.com>
2021-01-28 16:17:09 +0530

    Added delete item functionality

--- src/App.js
+++ src/App.js
@@ -11,31 +11,60 @@ class App extends Component {
     }
   }
 
-  addItem(){
+  updateInput(key,value){
+    this.setState({
+      [key]:value
+    })
+  }
+
+  addItem() {
     const newItem = {
-      id = ! + Math.random(),
-      value = this.state.newItem.slice()
-    }
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+ 
+    };
 
-    const list = [...this.state.list]
+    const list = [...this.state.list];
 
-    list.push(newItem)
+    list.push(newItem);
 
     this.setState({
       list,
-      newItem:""
-    })
+      newItem: ""
+    });
+
+    console.log(this.state.newItem)
   }
+
+  deleteItem(id){
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id != id)
+
+    this.setState({list: updatedList});
+  }
+  
   render(){
     return (
       <div className={classes.App}>
         <div>
           <p>Add an item</p>
           <br/>
-          <input type="text" placeholder="Enter a task" 
+          <input type="text" placeholder="Enter a task" value={this.state.newItem}
           onChange={e=> this.updateInput("newItem",e.target.value)}
           />
           <button onClick={()=>this.addItem()}>Add</button>
+          <br/>
+          <ul>
+            {this.state.list.map(item=>{
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={()=>{this.deleteItem(item.id)}}>X</button>
+                  </li>
+              )
+            })}
+          </ul>
         </div>
       </div>
     );
